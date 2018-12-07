77512bb99c0bd9418fe53fde09ab2ad78193726c
tianyu_pc <tianyu_pc@tianyu-pcs-MacBook-Pro.local>
2018-12-06 23:59:18 -0500

    chapter 4 learned nothing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -13,7 +13,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title","#{@base_title}"
   end
 
   test "should get help" do
@@ -28,9 +28,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "About | #{@base_title}"
   end
 
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
-  end
 end
